6bf3c606ab0dc7063c2b41b675dbf9bc098e5c08
makcext <makcbowka@gmail.com>
2023-07-29 21:31:42 +0300

    edu block update

--- src/components/PaperMid/index.tsx
+++ src/components/PaperMid/index.tsx
@@ -9,6 +9,7 @@ interface Image {
 }
 
 const auebPng = require('./aueb.png');
+
 const images: Image[] = [
 	{ id: 1, text: 'Athens Univercity of Economics and Business' },
   { id: 2, src: auebPng },
@@ -16,32 +17,34 @@ const images: Image[] = [
 ];
 
 function ImageGrid() {
-  console.log(images[0].src);
 
   return (
 		<>
-		<Box display="block" justifyContent="Left" textAlign={'left'}>
+		<Box display="block" justifyContent="Left" textAlign={'left'} >
+      
+      <Paper elevation={4}>
+      <Paper variant="outlined" sx={{ borderRadius: '16px' }}>
 			<Typography variant='h3' > Education </Typography>
-			<Typography variant='h5' > Bachelor of Science in Computer Science </Typography>
-		</Box>
-		
-	
+			<Typography variant='h5' > Bachelor of Computer Science </Typography>
 
-		<Box sx={{ height: '100%' }}>
-    <Grid container spacing={0}  justifyItems={"center"} alignItems={"center"} sx={{ height: '100%' }}>
-      {images.map(image => (
-        <Grid item key={image.id}  xs={6} sm={12} p={1} height={"100%"}>
-          <Paper elevation={4} sx={{ display: 'flex', justifyContent: 'center', padding:'8px' }}>
-            {image.src ? (
-              <img src={image.src} alt='aueb logo' style={{ height: '100%', width: '100%' }} />
-            ) : (
-              <Typography variant="body2" align="center">{image.text}</Typography>
-            )}
-          </Paper>
-					
-        </Grid>
-      ))}
-    </Grid>
+      <Grid container spacing={0}  justifyItems={"center"} alignItems={"center"} sx={{ height: '100%' }}>
+        
+        {images.map(image => (
+          <Grid item key={image.id}  xs={6} sm={12} p={1} height={"100%"}>
+            <Paper variant={"outlined"} elevation={4}  sx={{ display: 'flex', justifyContent: 'center', padding:'8px' }}>
+              {image.src ? (
+                <img src={image.src} alt='aueb logo' style={{ height: '100%', width: '100%' }} />
+              ) : (
+                <Typography variant="body2" align="center">{image.text}</Typography>
+              )}
+            </Paper>
+            
+          </Grid>
+        ))}
+      </Grid>
+      </Paper>
+      </Paper>
+      
 		</Box>
 		</>
   );
