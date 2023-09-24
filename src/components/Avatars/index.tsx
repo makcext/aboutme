2cf4a87763580c65ef924a9ce786f565bf0edb21
makcext <makcbowka@gmail.com>
2023-09-24 13:16:42 +0300

    avatar & stream fine tuning ui

--- src/components/Avatars/index.tsx
+++ src/components/Avatars/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createAvatar } from '@dicebear/avatars';
 import * as style from '@dicebear/avatars-avataaars-sprites';
 import { Button, Grid, Paper, Typography } from '@mui/material';
@@ -6,57 +6,61 @@ import Box from '@mui/material/Box';
 
 const Avatar = () => {
   const [index, setIndex] = useState(0);
+  const [avatars, setAvatars] = useState(Array.from({ length: 5 }, () => createAvatar(style, { seed: Math.random().toString() })));
 
   const handlePrev = () => {
-    setIndex((index - 1 + 5) % 5);
+    setIndex((index - 1 + avatars.length) % avatars.length);
   };
 
   const handleNext = () => {
-    setIndex((index + 1) % 5);
+    setIndex((index + 1) % avatars.length);
   };
 
-	const avatars = Array.from({ length: 5 }, () => createAvatar(style, { seed: Math.random().toString() }));
-	
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((index + 1) % avatars.length);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [index, avatars.length]);
+
   return (
-
-
-    <Box paddingBottom={1} justifyContent="space-around" >
+    <Box paddingBottom={1} justifyContent="space-around">
       <Paper variant="outlined" sx={{ borderColor: 'gray', padding: 1 }}>
-        <Typography variant="h4" >Avatars</Typography>
-
-
-    {/* <div style={{ flexGrow: 1, padding: 16, display: 'flex', justifyContent: 'center', alignItems: 'center' }}> */}
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          {/* <Typography variant="h4" align="center">
-            Avatars Slideshow
-          </Typography> */}
+        <Typography variant="h4">Avatars</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Paper style={{ padding: 16, textAlign: 'center', color: 'text.secondary' }}>
+              <div style={{ width: '150px', height: '150px', margin: '0 auto' }} dangerouslySetInnerHTML={{ __html: avatars[index] }} />
+            </Paper>
+          </Grid>
+          {/* <Grid item xs={12}>
+            <Button variant="outlined" color="warning" onClick={handlePrev}>
+              Prev
+            </Button>
+            <Button variant="outlined" color="warning" onClick={handleNext}>
+              Next
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="outlined" color="error" onClick={() => {
+              const newAvatars = [...avatars];
+              newAvatars.splice(index, 1);
+              setAvatars(newAvatars);
+              setIndex(Math.min(index, newAvatars.length - 1));
+            }}>
+              Delete
+            </Button>
+            <Button variant="outlined" color="success" onClick={() => {
+              const newAvatar = createAvatar(style, { seed: Math.random().toString() });
+              setAvatars([...avatars, newAvatar]);
+              setIndex(avatars.length);
+            }}>
+              Add
+            </Button>
+          </Grid> */}
         </Grid>
-        <Grid item xs={12}>
-          <Paper style={{ padding: 16, textAlign: 'center', color: 'text.secondary' }}>
-            <div style={{ width: '250px', height: '250px', margin: '0 auto' }} dangerouslySetInnerHTML={{ __html: avatars[index] }} />
-          </Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Button  variant="outlined" color="warning" onClick={handlePrev}>
-            Prev
-          </Button>
-          <Button variant="outlined" color="warning" onClick={handleNext}>
-            Next
-          </Button>
-        </Grid>
-      </Grid>
-    {/* </div> */}
-
-
-		</Paper>
-		</Box>
-      
- 
-
-
-
-
+      </Paper>
+    </Box>
   );
 };
 
