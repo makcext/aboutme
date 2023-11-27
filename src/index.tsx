71a726d11c2662d62e8e0549b6ffd28af216524e
makcext <makcbowka@gmail.com>
2023-11-27 03:28:59 +0200

    build

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,15 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 
-// const client = new ApolloClient({
-//   uri: 'https://abtm-c97ea3f9a33e.herokuapp.com/status',
-//   cache: new InMemoryCache()
-// });
- 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: 'https://abtm-c97ea3f9a33e.herokuapp.com/status',
   cache: new InMemoryCache()
 });
+ 
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000/',
+//   cache: new InMemoryCache()
+// });
 
 //
 
