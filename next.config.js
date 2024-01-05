18fd9fb62b6c92cfa8149011c739a587cf61e9f1
makcext <makcbowka@gmail.com>
2024-01-05 22:52:47 +0200

    path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   distDir: 'build-next',
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return defaultPathMap;
-  },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/{master}' : '',
 };
