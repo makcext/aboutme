516aa0778294ac399c0688be409b42dbc3f1a5de
makcext <makcbowka@gmail.com>
2024-01-26 09:05:47 +0200

    axios to fetch

--- app/woltApi/actions.tsx
+++ app/woltApi/actions.tsx
@@ -1,22 +1,16 @@
-
+import axios from 'axios';
 
 async function getData() {
-  'use server'
   try {
-    const res = await fetch('https://restaurant-api.wolt.com/v1/pages/restaurants?lat=60.170187&lon=24.930599')
-    if (!res.ok) {
-      throw new Error('Failed to fetch data')
-    }
-    return res.json()
+    const res = await axios.get('https://restaurant-api.wolt.com/v1/pages/restaurants?lat=60.170187&lon=24.930599');
+    return res.data;
   } catch (error) {
     console.error('An error occurred:', error);
     throw error;
   }
 }
- 
-export default async function WoltApiFetch() {
-  const data = await getData()
- 
-  return data
-}
 
+export default async function WoltApiFetch() {
+  const data = await getData();
+  return data;
+}
