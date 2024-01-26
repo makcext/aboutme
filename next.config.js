377a28338f66812f1d00e9836a1524f21fc93dcb
makcext <makcbowka@gmail.com>
2024-01-26 08:33:16 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	output: 'export',
-	images: {
-		unoptimized: true
-	},
 	basePath: process.env.NODE_ENV === 'production' ? '/aboutme' : '',
 	assetPrefix: process.env.NODE_ENV === 'production' ? '/aboutme' : '',
-	
 }
 
 module.exports = nextConfig
