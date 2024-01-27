61721d63207eb20009be90eaaa2ca71886cdd539
makcext <makcbowka@gmail.com>
2024-01-27 22:56:20 +0200

    all restaurants

--- app/woltApi/page.tsx
+++ app/woltApi/page.tsx
@@ -11,10 +11,16 @@ import CardMedia from '@mui/material/CardMedia';
 import { Paper, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Switch from '@mui/material/Switch';
+import Badge from '@mui/material/Badge';
+import Tooltip from '@mui/material/Tooltip';
+import Divider from '@mui/material/Divider';
+import MopedIcon from '@mui/icons-material/Moped';
+import MopedOutlinedIcon from '@mui/icons-material/MopedOutlined';
 
 import FetchData from './ApiCall'
 // import FetchData from './ApiCallNext'
 import FilterPrice from './filterPrice';
+import { Margin } from '@mui/icons-material';
 
 interface Filter {
 	name: string;
@@ -40,11 +46,6 @@ export default async function Page() {
 
 
 					<Typography variant='h3' p={2}>Categories</Typography>
-
-
-
-
-
 					<Grid style={{ display: 'flex', overflowX: 'auto', scrollbarWidth: 'none', msOverflowStyle: 'none', padding: 8 }}>
 						{data.sections[0].items.map((item: any) => (
 							<Card elevation={3} key={item.condent_id} style={{ flex: '0 0 auto', marginRight: '8px', width: '128px' }}>
@@ -66,6 +67,84 @@ export default async function Page() {
 						))}
 					</Grid>
 
+					<Typography variant='h4' p={2}>{data.sections[1].title}</Typography>
+					{data.sections[1].items.slice(0, 5).map((item: any, index: any) => (
+						<Grid p={1} item key={item.content_id} style={{ flexShrink: 0, }}>
+							<Card elevation={3}>
+								{/* <CardHeader
+									title={<Typography variant="body2">{item.title}</Typography>}
+								/> */}
+								<CardMedia
+									component="img"
+									alt={item.title}
+									height="254"
+									image={item.image.url}
+								/>
+
+								<CardContent style={{ paddingBottom: 0 }}>
+
+
+									<Grid container spacing={2}>
+
+										<Grid item xs={9}>
+											<Typography variant="overline" >
+												{item.title}
+											</Typography>
+
+
+
+											<Typography variant="body2" color="textSecondary" >
+												{item.venue.short_description?.substring(0, 64)}
+											</Typography>
+										</Grid>
+
+
+										<Grid item alignContent={'center'} xs={3}>
+
+											<Box>
+												<Chip color='primary' sx={{
+													height: 'auto',
+													'& .MuiChip-label': {
+														display: 'block',
+														whiteSpace: 'normal',
+
+													},
+												}}
+
+
+													label={
+														<>
+															<Typography align="center">{`${item.venue.estimate_range}`}</Typography>
+															<Typography align="center">{`${item.venue.estimate_box.subtitle}`}</Typography>
+														</>
+													} />
+
+
+											</Box>
+
+										</Grid>
+
+
+									</Grid>
+									<br />
+									{/* <Divider variant="fullWidth"  /> */}
+
+									<Box display="flex" alignItems="left" justifyContent="left">
+										<MopedOutlinedIcon />
+										<Typography variant='body1' marginRight={1}>
+											{item.venue.delivery_price}
+										</Typography>
+
+										<Typography variant="body1" color="textSecondary" align='center' >
+											{item.venue.rating ? item.venue.rating.score : 'No rating'}
+										</Typography>
+									</Box>
+
+
+								</CardContent>
+							</Card>
+						</Grid>
+					))}
 
 
 
