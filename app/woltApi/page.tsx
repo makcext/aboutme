52b4ae3a859956b9be85678de82e67dab809ec3e
makcext <makcbowka@gmail.com>
2024-01-28 00:25:05 +0200

    ui/ux

--- app/woltApi/page.tsx
+++ app/woltApi/page.tsx
@@ -19,29 +19,22 @@ import MopedOutlinedIcon from '@mui/icons-material/MopedOutlined';
 
 import FetchData from './ApiCall'
 // import FetchData from './ApiCallNext'
-import FilterPrice from './filterPrice';
-import { Margin } from '@mui/icons-material';
-
-interface Filter {
-	name: string;
-	// add other properties here as needed
-}
+// import FilterPrice from './filterPrice';
 
 
 export default async function Page() {
 	const data = await FetchData()
 
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
+		<Suspense fallback={<div>Loading Restaurants...</div>}>
 
 			<div>
 				<Box m={1}>
 					<h1>Wolt API restaurants</h1>
 					<Typography variant='h4' p={2}>{data.sections[0].title}</Typography>
-					<Typography variant='h4' p={2}>Categories</Typography>
 					<Grid style={{ display: 'flex', overflowX: 'auto', scrollbarWidth: 'none', msOverflowStyle: 'none', padding: 8 }}>
-						{data.sections[0].items.map((item: any) => (
-							<Card elevation={3} key={item.condent_id} style={{ flex: '0 0 auto', marginRight: '8px', width: '128px' }}>
+						{data.sections[0].items.map((item: any, index: any) => (
+							<Card elevation={3} key={index} style={{ flex: '0 0 auto', marginRight: '8px', width: '128px' }}>
 								<CardMedia
 									component="img"
 									alt={item.title}
@@ -62,8 +55,9 @@ export default async function Page() {
 
 
 					<Typography variant='h4' p={2}>{data.sections[1].title}</Typography>
+
 					{data.sections[1].items.slice(0, 5).map((item: any, index: any) => (
-						<Grid p={1} item key={item.content_id} style={{ flexShrink: 0, }}>
+						<Grid p={1} item key={index}>
 							<Card elevation={3}>
 								<CardMedia
 									component="img"
@@ -73,26 +67,26 @@ export default async function Page() {
 								/>
 
 								<CardContent style={{ paddingBottom: 0 }}>
-								<Typography variant="overline" >
+								<Typography variant="h6" >
 												{item.title}
 											</Typography>
-									<Grid container spacing={2}>
+
+									<Grid container alignItems={'space-between'}>
+
 										<Grid item xs={9}>
-											<Typography variant="body2" color="textSecondary" >
-												{item.venue.short_description?.substring(0, 64)}
+											<Typography variant="body2" color="textSecondary" style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+												{item.venue.short_description}
 											</Typography>
 										</Grid>
-										<Grid item alignContent={'center'} xs={3}>
-											<Box>
-												<Chip style={{ backgroundColor: 'rgba(0, 157, 224, 0.08)' }}  sx={{height: 'auto', '& .MuiChip-label': {display: 'block', whiteSpace: 'normal',},}}
-													label={
-														<>
-															<Typography variant='body2' align="center">{`${item.venue.estimate_range}`}</Typography>
-															<Typography variant='body2' align="center">{`${item.venue.estimate_box.subtitle}`}</Typography>
-														</>
-													} />
-
-											</Box>
+										<Grid item xs={3} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+											<Chip style={{ backgroundColor: 'rgba(0, 157, 224, 0.08)' }}  sx={{height: 'auto', '& .MuiChip-label': {display: 'block', whiteSpace: 'normal',},}}
+												label={
+													<>
+														<Typography variant='body2' align="center"> {` ${item.venue.estimate_range} `} </Typography>
+														<Typography variant='body2' align="center"> {` ${item.venue.estimate_box.subtitle} `} </Typography>
+													</>
+												} 
+											/>
 										</Grid>
 									</Grid>
 
@@ -112,7 +106,7 @@ export default async function Page() {
 						</Grid>
 					))}
 
-					<h4>filter by €</h4>
+					{/* <h4>filter by €</h4>
 					<FilterPrice data={data} />
 
 
@@ -123,7 +117,7 @@ export default async function Page() {
 								<Chip variant="outlined" color="success" label={filter.name} />
 							</Box>
 						))
-					}
+					} */}
 
 
 
