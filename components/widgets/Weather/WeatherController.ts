72fd0fb5d8f7ffd743954ff10c868f210bf1d342
makcext <makcbowka@gmail.com>
2024-01-30 13:13:00 +0200

    wp: env

--- components/widgets/Weather/WeatherController.ts
+++ components/widgets/Weather/WeatherController.ts
@@ -1,14 +1,17 @@
+// "use client"
 // WeatherController.ts
 import axios from 'axios';
 import WeatherData from './WeatherModel';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_OPENWEATHER;
+// const API_KEY = "149da3eb9110b7e6ba636b8ec43ac79a"
+// console.log(API_KEY);
 
 const fetchWeatherData = async (latitude: number = 37.98, longitude: number = 23.72): Promise<WeatherData> => {
 	try {
+		// const API_KEY = process.env.OPENWEATHER;
 		const response = await axios.get<WeatherData>(
 			`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
 		);
