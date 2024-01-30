a9db78c3aff1a80b25c5d90ac56669b118bb7af4
makcext <makcbowka@gmail.com>
2024-01-30 20:41:44 +0200

    wp: env

--- components/ApolloWrapper.tsx
+++ components/ApolloWrapper.tsx
@@ -14,7 +14,7 @@ import {
 function makeClient() {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "https://abtm-c97ea3f9a33e.herokuapp.com/status",
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL,
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
--- components/useApollo.tsx
+++ components/useApollo.tsx
@@ -1,26 +1,27 @@
-"use client";
-import { useMemo } from 'react';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import dotenv from 'dotenv';
+// "use client";
+// import { useMemo } from 'react';
+// import { ApolloClient, InMemoryCache } from '@apollo/client';
+// import dotenv from 'dotenv';
 
 
 
-dotenv.config();
+// dotenv.config();
 
 
-export default function useApollo(initialState = null) {
-	const client = useMemo(() => {
-		console.log('Initializing Apollo Client');
-		return new ApolloClient({
-			uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL, // replace with your GraphQL server URL
-			cache: new InMemoryCache(),
-		});
-	}, []);
+// export default function useApollo(initialState = null) {
+// 	const client = useMemo(() => {
+// 		console.log('Initializing Apollo Client');
+// 		return new ApolloClient({
+// 			uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL, // replace with your GraphQL server URL
+// 			cache: new InMemoryCache(),
+// 		});
+// 	}, []);
 
-	if (initialState) {
-		console.log('Restoring initial state');
-		client.cache.restore(initialState);
-	}
+// 	if (initialState) {
+// 		console.log('Restoring initial state');
+// 		client.cache.restore(initialState);
+// 	}
 
-	return client;
-}
+// 	return client;
+// }
+// now with next 14 this code not supported. used ApolloWrapper.tsx instead
