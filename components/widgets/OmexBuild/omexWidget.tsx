3630216a9b5fcfbcd144dae0b3980fee305f2eae
makcext <makcbowka@gmail.com>
2024-04-16 19:01:38 +0300

    fix

--- components/widgets/OmexBuild/omexWidget.tsx
+++ components/widgets/OmexBuild/omexWidget.tsx
@@ -62,93 +62,43 @@ const projects = [
 
 
 const OmexWidget = () => {
-
-
 	const [currentImage, setCurrentImage] = useState(Array(projects.length).fill(0));
 
 	const handleScroll = (index: any) => (event: any) => {
 		const element = event.target;
 		const visibleImageIndex = Math.ceil(element.scrollLeft / element.clientWidth);
 		setCurrentImage((prev) =>
-			prev.map((item, i) => i === index ? visibleImageIndex : item)
+			prev.map((item, i) => (i === index ? visibleImageIndex : item))
 		);
 	};
 
-
-
-
-
-
 	return (
 		<>
-
-			<Box maxWidth="xs" paddingTop={0} justifyContent="space-around" textAlign="left" minHeight={460}>
-				<Paper elevation={3} >
-					<Paper variant="outlined" sx={{ borderColor: 'gray', padding: 1 }}>
-						<Box textAlign="left" minHeight={460}>
-							<Typography variant="body1">15 Apr 2024 Finished Omex Build Project: [ feb 2024 - apr 2024 ]</Typography>
-							{/* <Typography variant="body2">Description: This is a project to build a web app for Omex Build ltd</Typography> */}
-							{/* <Typography variant="body2">Stack: nextjs reactjs </Typography> */}
-							<Typography variant="body2">Functional Requirements: contact form, customer review, scanning qr code notification to mail </Typography>
-
-
-
-							{projects.map((project, index) => (
-								<Grid xs={12} md={6} item key={project.id}>
-									<Box component='article' sx={{ width: '100%', height: '100%', position: 'relative', p: 1 }}>
-										<Card elevation={4} sx={{ backgroundColor: 'transparent' }}>
-											<Box sx={{ display: 'flex', overflowY: 'auto', scrollSnapType: 'x mandatory', scrollbarWidth: '-moz-initial', '&::-webkit-scrollbar': { display: 'none' } }} onScroll={handleScroll(index)}>
-												{projects[0].images.map((image, index) => (
-													<Card key={index} sx={{width: '280', height: '360'}}>
-														<Image src={`aboutme/omex/${image.url}.png`} alt={project.title} width={280} height={360} />
-														
-													</Card>
-												))}
-
-
-                     {/* <Image src={`/aboutme/weatherIcons/${item?.weather[0]?.icon || 'unknown'}.png`} alt={weatherData?.weather[0]?.description || ''} width={48} height={48} /> */}
-
-
-
-
-											</Box>
-											<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 0 }}>
-												{project.images.map((_, dotIndex) => (
-													<Box key={dotIndex} sx={{ width: 6, height: 6, borderRadius: '50%', backgroundColor: dotIndex === currentImage[index] ? 'primary.main' : 'grey.500', mx: 0.5 }} />
-												))}
-											</Box>
-											<Box p={1} display={'flex'} justifyContent={'space-between'}>
-												<Typography component='h3' variant='h2' style={{ fontSize: '0.8rem' }} fontWeight={300} alignContent='center'>
-													{project.title} {project.street}
-												</Typography>
-											</Box>
-										</Card>
-									</Box>
-
-								</Grid>
-
-							))}
-
-
-
-
-
-
-
-
-
-						</Box>
-
-
-
-
-					</Paper>
-				</Paper>
-			</Box>
-
+			{projects.map((project, index) => (
+				<Grid xs={12} md={6} item key={project.id}>
+					<Box component='article' sx={{ width: '100%', height: '100%', position: 'relative', p: 2 }}>
+						<Card elevation={4} sx={{ backgroundColor: 'transparent' }}>
+							<Box sx={{ display: 'flex', overflowY: 'auto', scrollSnapType: 'x mandatory', scrollbarWidth: '-moz-initial', '&::-webkit-scrollbar': { display: 'none' } }} onScroll={handleScroll(index)}>
+								{project.images.map((image, imageIndex) => (
+									<CardMedia key={imageIndex} component="img" height="320px" image={`aboutme/omex/${project.images[currentImage[index]].url}.png`} alt={project.title} sx={{ flex: 'none', scrollSnapAlign: 'start', objectFit: 'cover' }} />
+								))}
+							</Box>
+							<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 1 }}>
+								{project.images.map((_, dotIndex) => (
+									<Box key={dotIndex} sx={{ width: 6, height: 6, borderRadius: '50%', backgroundColor: dotIndex === currentImage[index] ? 'primary.main' : 'grey.500', mx: 0.5 }} />
+								))}
+							</Box>
+							<Box p={2} display={'flex'} justifyContent={'space-between'}>
+								<Typography component='h3' variant='h2' style={{ fontSize: '1.4rem' }} fontWeight={300} alignContent='center'>
+									{project.title}, {project.street}
+								</Typography>
+							</Box>
+						</Card>
+					</Box>
+				</Grid>
+			))}
 		</>
 	);
 }
-
 
 export default OmexWidget;
