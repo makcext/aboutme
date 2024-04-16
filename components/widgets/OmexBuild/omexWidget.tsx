776081eb162d7431742f1d910d830410d2ef7b6b
makcext <makcbowka@gmail.com>
2024-04-16 19:20:40 +0300

    image ok

--- components/widgets/OmexBuild/omexWidget.tsx
+++ components/widgets/OmexBuild/omexWidget.tsx
@@ -39,12 +39,12 @@ const projects = [
 	{
 		id: 0,
 		images: [
-			{ id: 0, url: '01' },
-			{ id: 1, url: '02' },
-			{ id: 2, url: '03' },
-			{ id: 3, url: '04' },
-			{ id: 4, url: '05' },
-			{ id: 5, url: '06' },
+			'01',
+			'02',
+			'03',
+			'04',
+			'05',
+			'06',
 		],
 		title: 'omex',
 		by: 1,
@@ -68,29 +68,42 @@ const OmexWidget = () => {
 		const element = event.target;
 		const visibleImageIndex = Math.ceil(element.scrollLeft / element.clientWidth);
 		setCurrentImage((prev) =>
-			prev.map((item, i) => (i === index ? visibleImageIndex : item))
+			prev.map((item, i) => i === index ? visibleImageIndex : item)
 		);
 	};
 
 	return (
 		<>
+
+
+
 			{projects.map((project, index) => (
 				<Grid xs={12} md={6} item key={project.id}>
-					<Box component='article' sx={{ width: '100%', height: '100%', position: 'relative', p: 2 }}>
+					<Box component='article' sx={{ width: '100%', height: '100%', position: 'relative', p: 0 }}>
+
 						<Card elevation={4} sx={{ backgroundColor: 'transparent' }}>
+							<Box p={2}  justifyContent={'space-between'}>
+
+								<Typography variant="body1">15 Apr 2024 Finished Omex Build Project: [ feb 2024 - apr 2024 ]</Typography>
+								{/* <Typography variant="body2">Description: This is a project to build a web app for Omex Build ltd</Typography> */}
+								{/* <Typography variant="body2">Stack: nextjs reactjs </Typography> */}
+								<Typography variant="body2">Functional Requirements: contact form, customer review, scanning qr code notification to mail </Typography>
+							</Box>
+
 							<Box sx={{ display: 'flex', overflowY: 'auto', scrollSnapType: 'x mandatory', scrollbarWidth: '-moz-initial', '&::-webkit-scrollbar': { display: 'none' } }} onScroll={handleScroll(index)}>
 								{project.images.map((image, imageIndex) => (
-									<CardMedia key={imageIndex} component="img" height="320px" image={`aboutme/omex/${project.images[currentImage[index]].url}.png`} alt={project.title} sx={{ flex: 'none', scrollSnapAlign: 'start', objectFit: 'cover' }} />
+									<CardMedia key={imageIndex} component="img" height="420px" image={`aboutme/omex/${image}.png`} alt={project.title} sx={{ flex: 'none', scrollSnapAlign: 'start', objectFit: 'contain' }} />
 								))}
 							</Box>
-							<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 1 }}>
+
+							<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 0 }}>
 								{project.images.map((_, dotIndex) => (
 									<Box key={dotIndex} sx={{ width: 6, height: 6, borderRadius: '50%', backgroundColor: dotIndex === currentImage[index] ? 'primary.main' : 'grey.500', mx: 0.5 }} />
 								))}
 							</Box>
-							<Box p={2} display={'flex'} justifyContent={'space-between'}>
-								<Typography component='h3' variant='h2' style={{ fontSize: '1.4rem' }} fontWeight={300} alignContent='center'>
-									{project.title}, {project.street}
+							<Box p={1} display={'flex'} justifyContent={'space-between'}>
+								<Typography component='h3' variant='body2' style={{ fontSize: '0.8rem' }} fontWeight={300} alignContent='center'>
+									{project.title} {project.street}
 								</Typography>
 							</Box>
 						</Card>
