71340d9f6163d7f56e0866eb91a403a5a843b2b3
makcext <makcbowka@gmail.com>
2024-05-09 04:03:45 +0300

    avatar fix

--- components/widgets/HelloBlock/HelloBlock.tsx
+++ components/widgets/HelloBlock/HelloBlock.tsx
@@ -94,7 +94,7 @@ const HelloBlock = () => {
 							<Image
 								placeholder="blur"
 								blurDataURL="data:image/gif;base64,..."
-								src={'/aboutme/avatar.webp'}
+								src={'/aboutme/avatar.png'}
 								alt="Avatar"
 								width={128}
 								height={96}
