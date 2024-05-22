a14c396389c9a393b97a1c84943cfbeab0cd2714
makcext <makcbowka@gmail.com>
2024-05-22 20:02:11 +0300

    ui ux

--- components/widgets/OmexBuild/omexWidget.tsx
+++ components/widgets/OmexBuild/omexWidget.tsx
@@ -1,37 +1,9 @@
-'use client'
-import React, { useEffect, ChangeEvent } from 'react'
-import { useState } from 'react'
-import Image from 'next/image'
-import { Alert, Box, Card, CardMedia, Paper, TextField, Typography, IconButton, Grid } from '@mui/material'
-import Script from 'next/script'
-
-const imageData = [
-  {
-    id: 0,
-    src: '/omex/01.png',
-    alt: 'Omex Build',
-  },
-  {
-    id: 1,
-    src: '/omex/02.png',
-    alt: 'Omex Build',
-  },
-  {
-    id: 2,
-    src: '/omex/03.png',
-    alt: 'Omex Build',
-  },
-  {
-    id: 3,
-    src: '/omex/04.png',
-    alt: 'Omex Build',
-  },
-  {
-    id: 4,
-    src: '/omex/05.png',
-    alt: 'Omex Build',
-  },
-]
+'use client';
+import React, { useEffect, ChangeEvent } from 'react';
+import { useState } from 'react';
+import Image from 'next/image';
+import { Alert, Box, Card, CardMedia, Paper, TextField, Typography, IconButton, Grid } from '@mui/material';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
 
 const projects = [
   {
@@ -42,66 +14,64 @@ const projects = [
     street: 'build',
     description: 'This is a description',
   },
-]
+];
 
 const OmexWidget = () => {
-  const [currentImage, setCurrentImage] = useState(Array(projects.length).fill(0))
+  const [currentImage, setCurrentImage] = useState(Array(projects.length).fill(0));
 
   const handleScroll = (index: any) => (event: any) => {
-    const element = event.target
-    const visibleImageIndex = Math.ceil(element.scrollLeft / element.clientWidth)
-    setCurrentImage((prev) => prev.map((item, i) => (i === index ? visibleImageIndex : item)))
-  }
+    const element = event.target;
+    const visibleImageIndex = Math.ceil(element.scrollLeft / element.clientWidth);
+    setCurrentImage((prev) => prev.map((item, i) => (i === index ? visibleImageIndex : item)));
+  };
 
   return (
     <>
       <Box maxWidth="xs" paddingTop={0} justifyContent="space-around" textAlign="left">
-        {/* <Paper elevation={3} > */}
-        <Paper variant="outlined" sx={{ borderColor: 'gray', padding: 0 }}>
-          <Box textAlign="left">
+        <Paper variant="outlined" sx={{ borderColor: 'gray', paddingTop: 0, paddingBottom: 0 }}>
+          <Box component="article" sx={{ width: '100%', height: '100%', p: 0 }}>
+            
+            <Grid container spacing={0} alignItems="center" justifyContent="space-between">
+              <Typography variant="body1" color="info.main" sx={{ paddingLeft: 1 }}>
+                Omex Build Project: [ feb 2024 - apr 2024 ]
+              </Typography>
+
+              <IconButton disabled >
+                  <SyncAltIcon  />
+                </IconButton>
+            </Grid>
+
             {projects.map((project, index) => (
-              <Grid xs={12} md={12} item key={project.id}>
-                <Box component="article" sx={{ width: '100%', height: '100%', position: 'relative', p: 0 }}>
-                  <Card elevation={4} sx={{ backgroundColor: 'transparent', maxHeight: 560 }}>
-                    <Box p={1} justifyContent={'space-between'}>
-                     
-                    <Typography variant="body1" color="info.main" sx={{paddingLeft: 1}}>
-                      Omex Build Project: [ feb 2024 - apr 2024 ]
+              <>
+                <Card elevation={4} sx={{ backgroundColor: 'transparent', minHeight: 500 }}>
+                  <Box p={1} justifyContent={'space-between'}>
+                    <Typography variant="body2">Functional Requirements: contact form, customer review, scanning qr code notification to mail </Typography>
+                  </Box>
+                  <Box sx={{ display: 'flex', overflowY: 'auto', scrollSnapType: 'x mandatory', scrollbarWidth: '-moz-initial', '&::-webkit-scrollbar': { display: 'none' } }} onScroll={handleScroll(index)}>
+                    {project.images.map((image, imageIndex) => (
+                      <CardMedia key={imageIndex} component="img" height="400px" image={`aboutme/omex/${image}.webp`} alt={project.title} sx={{ flex: 'none', scrollSnapAlign: 'start', objectFit: 'contain' }} />
+                    ))}
+                  </Box>
+
+                  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 0 }}>
+                    {project.images.map((_, dotIndex) => (
+                      <Box key={dotIndex} sx={{ width: 6, height: 6, borderRadius: '50%', backgroundColor: dotIndex === currentImage[index] ? 'primary.main' : 'grey.500', mx: 0.5 }} />
+                    ))}
+                  </Box>
+
+                  <Box p={1} display={'flex'} justifyContent={'space-between'}>
+                    <Typography component="h3" variant="body2" style={{ fontSize: '0.8rem' }} fontWeight={300} alignContent="center">
+                      {project.title} {project.street}
                     </Typography>
-                     
-                      {/* <Typography variant="body1"> Omex Build Project: [ feb 2024 - apr 2024 ]</Typography> */}
-                     
-                      {/* <Typography variant="body2">Description: This is a project to build a web app for Omex Build ltd</Typography> */}
-                      {/* <Typography variant="body2">Stack: nextjs reactjs </Typography> */}
-                      <Typography variant="body2">Functional Requirements: contact form, customer review, scanning qr code notification to mail </Typography>
-                    </Box>
-
-                    <Box sx={{ display: 'flex', overflowY: 'auto', scrollSnapType: 'x mandatory', scrollbarWidth: '-moz-initial', '&::-webkit-scrollbar': { display: 'none' } }} onScroll={handleScroll(index)}>
-                      {project.images.map((image, imageIndex) => (
-                        <CardMedia key={imageIndex} component="img" height="420px" image={`aboutme/omex/${image}.webp`} alt={project.title} sx={{ flex: 'none', scrollSnapAlign: 'start', objectFit: 'contain' }} />
-                      ))}
-                    </Box>
-
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 0 }}>
-                      {project.images.map((_, dotIndex) => (
-                        <Box key={dotIndex} sx={{ width: 6, height: 6, borderRadius: '50%', backgroundColor: dotIndex === currentImage[index] ? 'primary.main' : 'grey.500', mx: 0.5 }} />
-                      ))}
-                    </Box>
-                    <Box p={1} display={'flex'} justifyContent={'space-between'}>
-                      <Typography component="h3" variant="body2" style={{ fontSize: '0.8rem' }} fontWeight={300} alignContent="center">
-                        {project.title} {project.street}
-                      </Typography>
-                    </Box>
-                  </Card>
-                </Box>
-              </Grid>
+                  </Box>
+                </Card>
+              </>
             ))}
           </Box>
         </Paper>
-        {/* </Paper> */}
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default OmexWidget
+export default OmexWidget;
--- components/widgets/Qtalents/QtWidget.tsx
+++ components/widgets/Qtalents/QtWidget.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import { Alert, Box, Card, CardMedia, Paper, TextField, Typography, IconButton, Grid, Link } from '@mui/material';
 import SyncAltIcon from '@mui/icons-material/SyncAlt';
 
-
 const projects = [
   {
     id: 0,
@@ -15,132 +14,97 @@ const projects = [
     street: 'Qtalents',
     description: 'This is a description',
   },
-]
+];
 
 const flip1 = () => {
   return (
     <Card elevation={4} sx={{ backgroundColor: 'transparent', minHeight: 500 }}>
-    <Box p={1} justifyContent={'space-between'} sx={{ height: '100%' }}>
-      <Typography component="h3" variant="h5">Software Developer</Typography>
-
-      <Link href="https://code.qtalents.co" underline="hover" color="success.main">
-        <Typography variant="body2" gutterBottom={true}>
-          in code.qtalents.co
+      <Box p={1} justifyContent={'space-between'} sx={{ height: '100%' }}>
+        <Typography component="h3" variant="h5">
+          Software Developer
         </Typography>
-      </Link>
 
-      <Typography component="p" variant="body1" gutterBottom={true} color={'primary.main'}>
-        Designed and developed new features to meet user requirements and needs. Researched modern technologies and innovative solutions to optimize development processes.
-      </Typography>
+        <Link href="https://code.qtalents.co" underline="hover" color="success.main">
+          <Typography variant="body1" gutterBottom={true}>
+            in code.qtalents.co
+          </Typography>
+        </Link>
 
-      <Typography component="h3" variant="subtitle1">Code Refactoring:</Typography>
-      <Typography component="p" variant="body2" gutterBottom={true} color={'primary.main'}>
-        Refactored existing code to improve readability, scalability, and maintainability.
-      </Typography>
+        <Typography component="p" variant="body1" gutterBottom={true} color={'primary.main'}>
+          Designed and developed new features to meet user requirements and needs. Researched modern technologies and innovative solutions to optimize development processes.
+        </Typography>
 
-      <Typography component="h3" variant="subtitle1">Debugging and Error Correction:</Typography>
-      <Typography component="p" variant="body2" gutterBottom={true} color={'primary.main'}>
-        Identified, analyzed, and resolved code errors to ensure system stability and smooth operation.
-      </Typography>
+        <Typography component="h3" variant="subtitle1">
+          Code Refactoring:
+        </Typography>
+        <Typography component="p" variant="body2" gutterBottom={true} color={'primary.main'}>
+          Refactored existing code to improve readability, scalability, and maintainability.
+        </Typography>
 
-      <Typography component="h3" variant="subtitle1">Implementation of New UI Components:</Typography>
-      <Typography variant="body2" gutterBottom={true} color={'primary.main'}>
-        Developed and integrated a new UI component that significantly enhanced user experience. Conducted testing and optimization of the new UI component.
-      </Typography>
-      <Typography component="h3" variant="subtitle1">Technical Skills:</Typography>
-      <Typography variant="body2" gutterBottom={true} color={'primary.main'}>
-        Programming languages: JavaScript Frameworks: Next.js Libraries: React Tools: Git
-      </Typography>
-    </Box>
-  </Card>
+        <Typography component="h3" variant="subtitle1">
+          Debugging and Error Correction:
+        </Typography>
+        <Typography component="p" variant="body2" gutterBottom={true} color={'primary.main'}>
+          Identified, analyzed, and resolved code errors to ensure system stability and smooth operation.
+        </Typography>
+
+        <Typography component="h3" variant="subtitle1">
+          Implementation of New UI Components:
+        </Typography>
+        <Typography variant="body2" gutterBottom={true} color={'primary.main'}>
+          Developed and integrated a new UI component that significantly enhanced user experience. Conducted testing and optimization of the new UI component.
+        </Typography>
+        <Typography component="h3" variant="subtitle1">
+          Technical Skills:
+        </Typography>
+        <Typography variant="body2" gutterBottom={true} color={'primary.main'}>
+          Programming languages: JavaScript Frameworks: Next.js Libraries: React Tools: Git
+        </Typography>
+      </Box>
+
+    </Card>
   );
 };
 
 const Flip2 = (currentImage: any, setCurrentImage: any) => {
-
-
   const handleScroll = (index: any) => (event: any) => {
     const element = event.target;
     const visibleImageIndex = Math.ceil(element.scrollLeft / element.clientWidth);
-    setCurrentImage((prev:any) => prev.map((item:any, i:any) => (i === index ? visibleImageIndex : item)));
+    setCurrentImage((prev: any) => prev.map((item: any, i: any) => (i === index ? visibleImageIndex : item)));
   };
 
   return (
+    <Card sx={{ maxHeight: 560 }}>
+      {projects.map((project, index) => (
+        <Grid xs={12} md={12} item key={project.id}>
+          <Box component="article" sx={{ width: '100%', height: '100%', position: 'relative', p: 0 }}>
+            <Card elevation={4} sx={{ minHeight: 500 }}>
+              <Box p={1} justifyContent={'space-between'}>
+                <Typography variant="body2">Designed and developed new features to meet user requirements and needs.  </Typography>
+              </Box>
 
-    <Card sx={{maxHeight: 560}}>
-    {projects.map((project, index) => (
-      <Grid xs={12} md={12} item key={project.id} >
-        <Box
-          component="article"
-          sx={{
-            width: '100%',
-            height: '100%',
-            position: 'relative',
-            p: 0,
-          }}>
-          <Card elevation={4} sx={{  minHeight: 500 }}>
-            <Box p={1} justifyContent={'space-between'}>
-              <Typography variant="body2">Functional Requirements: contact form, customer review, scanning qr code notification to mail </Typography>
-            </Box>
+              <Box sx={{ display: 'flex', overflowY: 'auto', scrollSnapType: 'x mandatory', scrollbarWidth: '-moz-initial', '&::-webkit-scrollbar': { display: 'none' } }} onScroll={handleScroll(index)}>
+                {project.images.map((image, imageIndex) => (
+                  <CardMedia key={imageIndex} component="img" height="400px" image={`aboutme/qt/${image}.webp`} alt={project.title} sx={{ flex: 'none', scrollSnapAlign: 'start', objectFit: 'contain' }} />
+                ))}
+              </Box>
 
-            <Box
-              sx={{
-                display: 'flex',
-                overflowY: 'auto',
-                scrollSnapType: 'x mandatory',
-                scrollbarWidth: '-moz-initial',
-                '&::-webkit-scrollbar': { display: 'none' },
-              }}
-              onScroll={handleScroll(index)}>
-              {project.images.map((image, imageIndex) => (
-                <CardMedia 
-                  key={imageIndex}
-                  component="img"
-                  height="400px"
-                  image={`aboutme/qt/${image}.webp`}
-                  alt={project.title}
-                  sx={{
-                    flex: 'none',
-                    scrollSnapAlign: 'start',
-                    objectFit: 'contain',
-                  }}
-                />
-              ))}
-            </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 0, paddingTop: 0 }}>
+                {project.images.map((_, dotIndex) => (
+                  <Box key={dotIndex} sx={{ width: 6, height: 6, borderRadius: '50%', backgroundColor: dotIndex === currentImage[index] ? 'info.main' : 'grey.500', mx: 0.5 }} />
+                ))}
+              </Box>
 
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                mt: 0,
-                paddingTop: 0,
-              }}>
-              
-              {project.images.map((_, dotIndex) => (
-                <Box
-                  key={dotIndex}
-                  sx={{
-                    width: 6,
-                    height: 6,
-                    borderRadius: '50%',
-                    backgroundColor: dotIndex === currentImage[index] ? 'info.main' : 'grey.500',
-                    mx: 0.5,
-                    
-                  }}
-                />
-              ))}
-            </Box>
-            <Box p={1} display={'flex'} justifyContent={'space-between'}>
-              <Typography component="h3" variant="body2" style={{ fontSize: '0.8rem' }} fontWeight={300} alignContent="center">
-                {project.title} {project.street}
-              </Typography>
-            </Box>
-          </Card>
-        </Box>
-      </Grid>
-    ))}
-  </Card>
+              <Box p={1} display={'flex'} justifyContent={'space-between'}>
+                <Typography component="h3" variant="body2" style={{ fontSize: '0.8rem' }} fontWeight={300} alignContent="center">
+                  {project.title} {project.street}
+                </Typography>
+              </Box>
+            </Card>
+          </Box>
+        </Grid>
+      ))}
+    </Card>
   );
 };
 
@@ -157,23 +121,19 @@ const QtWidget = () => {
         {/* <Paper elevation={3} > */}
         <Paper variant="outlined" sx={{ borderColor: 'gray', padding: 0 }}>
           <Box textAlign="left">
-            <Box>
-              <Grid container spacing={0} alignItems="center" justifyContent="space-between">
-                
-                <Typography variant="body1" color="info.main" sx={{paddingLeft: 1}}>
-                  Experience [ Apr 2022 - Present ]
-                </Typography>
+            {/* <Box> */}
+            <Grid container spacing={0} alignItems="center" justifyContent="space-between">
+              <Typography variant="body1" color="info.main" sx={{ paddingLeft: 1 }}>
+                Experience [ Apr 2022 - Present ]
+              </Typography>
 
+              <IconButton onClick={handleIconButtonPress}>
+                <SyncAltIcon color="success" />
+              </IconButton>
+            </Grid>
 
-
-
-                <IconButton onClick={handleIconButtonPress}>
-                  <SyncAltIcon color="success" />
-                </IconButton>
-              </Grid>
-
-              {isFlipped ? Flip2(currentImage, setCurrentImage) : flip1()} 
-            </Box>
+            {isFlipped ? Flip2(currentImage, setCurrentImage) : flip1()}
+            {/* </Box> */}
           </Box>
         </Paper>
       </Box>
